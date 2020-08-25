27034d1b1e8f4a1301fb8c2d54be00d9f3f24957
sumansjs <suman.hansada@gmail.com>
2020-08-25 22:10:50 +0530

    functions as first class citizens

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,1 +1,32 @@
-console.log("Hello World!");
+// In JavaScript Functions are First Class Citizens
+// It means, we can -
+
+// * Assign it to a variable
+
+function sayHello() {
+    return "Hello World";
+}
+
+let fn = sayHello;
+// fn(); is same as sayHello();
+
+// * Pass as an argument
+
+function greet(fnMessage) {
+    console.log(fnMessage());
+}
+
+greet(sayHello);
+// passing the reference of sayHello to greet
+
+// * Return from other functions
+
+function sayHelloAgain() {
+    return function() {
+        return "Hello World";
+    }
+}
+
+let fnAgain = sayHelloAgain;
+let message = fnAgain();
+// returning anonymous function from sayHelloAgain
