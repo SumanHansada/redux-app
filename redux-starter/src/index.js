14862a9014c09dacf7ea65d2ce3d7376e87b2fbc
sumansjs <suman.hansada@gmail.com>
2020-08-26 08:04:07 +0530

    composing and piping

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,3 +1,5 @@
+import {compose, pipe} from "lodash/fp";
+
 let input = "   JavaScript   ";
 let output = "<div>" + input.trim() + "</div>";
 
@@ -6,7 +8,21 @@ const trim = str => str.trim();
 const wrapInDiv = str => `<div>${str}</div>`;
 const toLowerCase = str => str.toLowerCase();
 
-const result = wrapInDiv(toLowerCase(trim(input)));
+
 // Problems -
 // * Need to read the expression from right to left
 // * So many brackets
+const result = wrapInDiv(toLowerCase(trim(input)));
+console.log(result);
+
+// Using Lodash compose
+// Still order of operation is from right to left
+const transform = compose(wrapInDiv, toLowerCase, trim);
+console.log(transform(input));
+
+// Using Lodash pipe
+// Order of operation is from left to right
+const transformAnother = pipe(trim, toLowerCase, wrapInDiv);
+console.log(transformAnother(input));
+
+
