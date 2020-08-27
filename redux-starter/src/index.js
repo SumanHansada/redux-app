420cd8801abab7be9a70de347c4dbc5bb8f8153c
sumansjs <suman.hansada@gmail.com>
2020-08-27 07:27:03 +0530

    pure functions and immutability

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -27,4 +27,36 @@ console.log(transform(input));
 const transformAnother = pipe(trim, toLowerCase, wrap("div"));
 console.log(transformAnother(input));
 
+// Pure Functions
+// If we call it every time with same argument, it will return same value
+// In Pure Functions, We cannot use
+// * Random values
+// * No current date/time
+// * No global states (DOM, files, db etc)
+// * No mutation of parameters
 
+// Benefits
+// * Self Documenting
+// * Easily Testable
+// * Concurrency
+// * Cacheable
+
+// Immutability
+// Once you create an object, it cannot be changed
+// You have to create a copy first and then change that copy
+
+// In Pure Functional Programming Language, Once you create the object, it cannot be changed directly
+// JavaScript is not a Pure Functional Programming Language
+
+// const => We can change the object, but we cannot reassign it
+
+// Why Immutability ?
+// * Predictability
+// * Faster Change Detection (Only need to compare references and not data)
+// * Concurrency
+
+// Cons
+// * Performance
+// * Memory Overhead
+
+// When you are building application with Redux, you should not mutate data.
