2afc5eb877683cac4d9e01e2307f8aa3501c6d0b
sumansjs <suman.hansada@gmail.com>
2020-08-29 17:35:06 +0530

    added private properties in custom store

---
+++ redux-starter/src/customStore.js
@@ -0,0 +1,14 @@
+function createStore()
+{
+    let state;
+    
+    function getState() {
+        return state;
+    }
+
+    return {
+        getState
+    }
+}
+
+export default createStore();
