bbb1bc2d4ee983630d9d0db3c82b3348b16d7037
sumansjs <suman.hansada@gmail.com>
2020-08-29 18:52:27 +0530

    enabled redux dev tools

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -9,6 +9,8 @@ const unsubscribe = store.subscribe(() => {
 
 // Dispatching Actions
 store.dispatch(bugAdded("Bug1"));
+store.dispatch(bugAdded("Bug2"));
+store.dispatch(bugAdded("Bug3"));
 
 store.dispatch(bugResolved(1));
 
