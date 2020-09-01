5d492cc99be186a94b6a671028cb517414fd9a85
sumansjs <suman.hansada@gmail.com>
2020-09-01 23:05:01 +0530

    creating middleware

--- redux-starter/src/store/configureStore.js
+++ redux-starter/src/store/configureStore.js
@@ -1,6 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reducer from './reducer';
+import logger from "./middleware/logger";
 
 export default function() {
-    return configureStore({ reducer });
+    return configureStore({ 
+        reducer, 
+        middleware: [logger] 
+    });
 }
+
+// If not using redux toolkit
+// import { createStore, applyMiddleware } from "redux";
+// import reducer from './reducer';
+
+// const store = createStore(reducer, applyMiddleware(logger));
---
+++ redux-starter/src/store/middleware/logger.js
@@ -0,0 +1,11 @@
+const logger = state => next => action => {
+    console.log(state);
+    console.log(next);
+    console.log(action);
+    // It is required to pass the action to next middleware i.e. reducer
+    next(action);
+}
+
+export default logger;
+// Applied currying principle in middleware
+// N => 1
