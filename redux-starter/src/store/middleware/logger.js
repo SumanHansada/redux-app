689d2f61176116301c74ebd19d6fd7caed67c287
sumansjs <suman.hansada@gmail.com>
2020-09-04 08:02:16 +0530

    added social tests

--- redux-starter/src/store/middleware/logger.js
+++ redux-starter/src/store/middleware/logger.js
@@ -1,11 +1,11 @@
-const logger = param => state => next => action => {
+const logger = (param) => (state) => (next) => (action) => {
     console.log("Logging", param);
     // console.log(state);
     // console.log(next);
     // console.log(action);
     // It is required to pass the action to next middleware i.e. reducer
-    next(action);
-}
+    return next(action);
+};
 
 export default logger;
 // Applied currying principle in middleware
--- redux-starter/src/store/tests/bugs.spec.js
+++ redux-starter/src/store/tests/bugs.spec.js
@@ -1,6 +1,18 @@
 import { addBug, bugAdded } from "./../bugs";
 import { apiCallBegan } from "../api";
+import configureStore from "../configureStore";
 
+// Integration Test
+describe("bugsSlice", () => {
+    it("should handle addBug action", async () => {
+        const store = configureStore();
+        const bug = { description: "a" };
+        await store.dispatch(addBug(bug));
+        expect(store.getState().entities.bugs.list).toHaveLength(1);
+    });
+});
+
+// Unit Test
 describe("bugsSlice", () => {
     describe("action creators", () => {
         it("addBug", () => {
