c3f140c60e87a642c0b2e46153ced94fcc22ae3e
sumansjs <suman.hansada@gmail.com>
2020-09-04 23:17:14 +0530

    writing clean tests

--- redux-starter/src/store/tests/bugs.spec.js
+++ redux-starter/src/store/tests/bugs.spec.js
@@ -4,18 +4,34 @@ import { addBug, bugAdded } from "./../bugs";
 import { apiCallBegan } from "../api";
 import configureStore from "../configureStore";
 
+let fakeAxios;
+let store;
+
+beforeEach(() => {
+    fakeAxios = new MockAdaptor(axios);
+    store = configureStore();
+});
+
+const bugsSlice = () => store.getState().entities.bugs;
+
 // Integration Test
 describe("bugsSlice", () => {
-    it("should handle addBug action", async () => {
+    it("should add the bug to the store if it's saved to the server", async () => {
         const bug = { description: "a" };
         const savedBug = { ...bug, id: 1 };
-
-        const fakeAxios = new MockAdaptor(axios);
         fakeAxios.onPost("/bugs").reply(200, savedBug);
 
-        const store = configureStore();
         await store.dispatch(addBug(bug));
-        expect(store.getState().entities.bugs.list).toContainEqual(savedBug);
+
+        expect(bugsSlice().list).toContainEqual(savedBug);
+    });
+    it("should not add the bug to the store if it's not saved to the server", async () => {
+        const bug = { description: "a" };
+        fakeAxios.onPost("/bugs").reply(500);
+
+        await store.dispatch(addBug(bug));
+
+        expect(bugsSlice().list).toHaveLength(0);
     });
 });
 
