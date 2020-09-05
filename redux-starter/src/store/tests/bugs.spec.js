e1ab604c4bcbbfe1cb0e17a6973ce7fc221a8a52
sumansjs <suman.hansada@gmail.com>
2020-09-05 08:38:02 +0530

    exercise - resolve bug test cases

--- redux-starter/src/store/tests/bugs.spec.js
+++ redux-starter/src/store/tests/bugs.spec.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import MockAdaptor from "axios-mock-adapter";
-import { addBug, bugAdded, getUnresolvedBugs } from "./../bugs";
+import { addBug, bugAdded, getUnresolvedBugs, resolveBug } from "./../bugs";
 import { apiCallBegan } from "../api";
 import configureStore from "../configureStore";
 
@@ -23,6 +23,26 @@ const createState = () => ({
 
 // Unit Test
 describe("bugsSlice", () => {
+    it("should mark the bug as resolved if it's saved to the server", async () => {
+        fakeAxios.onPatch("/bugs/1").reply(200, { id: 1, resolved: true });
+        fakeAxios.onPost("/bugs").reply(200, { id: 1 });
+
+        await store.dispatch(addBug());
+        await store.dispatch(resolveBug(1));
+
+        expect(bugsSlice().list[0].resolved).toBe(true);
+    });
+
+    it("should not mark the bug as resolved if it's not saved to the server", async () => {
+        fakeAxios.onPatch("/bugs/1").reply(500);
+        fakeAxios.onPost("/bugs").reply(200, { id: 1 });
+
+        await store.dispatch(addBug());
+        await store.dispatch(resolveBug(1));
+
+        expect(bugsSlice().list[0].resolved).not.toBe(true);
+    });
+
     it("should add the bug to the store if it's saved to the server", async () => {
         const bug = { description: "a" };
         const savedBug = { ...bug, id: 1 };
